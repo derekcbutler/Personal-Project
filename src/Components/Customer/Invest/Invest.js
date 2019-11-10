297d32106926b3d5fb12ba659808a26c4ae2cda3
Derek Butler <derekbutler@Dereks-MacBook-Pro.local>
2019-11-10 14:07:23 -0700

    investPage

--- src/Components/Customer/Invest/Invest.js
+++ src/Components/Customer/Invest/Invest.js
@@ -1,32 +1,69 @@
 import React from "react";
+import axios from 'axios';
+
 import "./Invest.css";
 
-function Invest() {
-  return (
-    <div id="body">
-      <div>
-        Invest
-        <div className="invest-inputs">
-          <div>
-            {'First name: '}
-            <input placeholder="First name" className="input-boxes" />
+class Invest extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name_first: "",
+      name_last: "",
+      phone: "",
+      email: "",
+      lead_status: "Pending"
+    };
+  }
+
+  componentDidMount() {}
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  submitButton = () => {
+    //!upon click it will push to Strip and also create new lead with the status of active
+    console.log(this.state)
+    axios.post("/api/leads", {
+      name_first: this.state.name_first,
+      name_last: this.state.name_last,
+      phone: this.state.phone,
+      email: this.state.email,
+      lead_status: this.state.lead_status
+    }).then(res => {
+      //!finish here
+    })
+  };
+
+  render() {
+    return (
+      <div id="body">
+        <div>
+          Invest
+          <div className="invest-inputs">
+            <div>
+              {"First name: "}
+              <input placeholder="First name" className="input-boxes" />
+            </div>
+            <div>
+              {"Last Name: "}
+              <input placeholder="Last name" className="input-boxes" />
+            </div>
+            <div>
+              {"Phone Number: "}
+              <input placeholder="Phone" className="input-boxes" />
+            </div>
+            <div>
+              {"Email: "}
+              <input placeholder="email" className="input-boxes" />
+            </div>
+            <button className="submit">Submit</button>
           </div>
-          <div>
-            {'Last Name: '}
-            <input placeholder="Last name" className="input-boxes" />
-          </div>
-          <div>
-            {'Phone Number: '}
-            <input placeholder="Phone" className="input-boxes" />
-          </div>
-          <div>
-            {'Email: '}
-            <input placeholder="email" className="input-boxes" />
-          </div>
-          <button className='submit'>Submit</button>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 export default Invest;
